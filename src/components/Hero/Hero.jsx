1ce4a750fe00b4ff75ac2e19756859f9f1f64139
RICARDO CANDRA SULEBAGANTI <ricardocandrasulebaganti@gmail.com>
2026-08-18 21:21:28 +0700

    fix: adalah

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
             bg-clip-text text-transparent
 
             /* Mobile */
-            top-[25%] text-[33vw]
+            top-[20%] text-[33vw]
 
             /* Desktop (original) */
             md:top-24 md:text-[280px] lg:text-[360px] xl:text-[410px]
